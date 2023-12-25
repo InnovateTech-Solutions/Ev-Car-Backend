de92cbe824a809d1b25b80e46d1f0620afa05371
Mohammad Sameer Almaghteh <modtech7677@gmail.com>
2023-12-25 03:22:55 +0300

    login api / create station api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 
@@ -24,7 +25,7 @@ router.post('/register', async (req, res) => {
     });
 
     await user.save();
-    console.log( process.env.JWT_SECRET);
+    console.log(process.env.JWT_SECRET);
     const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN,
     });
@@ -42,26 +43,42 @@ router.post('/register', async (req, res) => {
   }
 });
 
+
 // Login and generate a JWT token
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', { session: false }, async (err, user, info) => {
-    if (err || !user) {
-      return res.status(401).json({ message: 'Authentication failed.' });
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    // If the user is not found or the password is incorrect, send an authentication failed response
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({
+        status: 'success',
+        token,
+        data: {
+          user,
+        },
+      });
     }
 
-    req.login(user, { session: false }, async (err) => {
-      if (err) {
-        res.send(err);
-      }
-
-      // Dynamically generate a new secret key for each login session
-      const secretKey = generateRandomKey();
-
-      // Use the dynamically generated secret key to sign the JWT token
-      const token = jwt.sign({ sub: user._id }, secretKey);
-      return res.json({ token });
+    const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
     });
-  })(req, res, next);
+
+    // Send the token in the response
+    return res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user,
+      },
+
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
 
 module.exports = router;
