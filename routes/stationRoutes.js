990265d10fdaa96ed5c571c871db03c9971e28a1
Mohammad Sameer Almaghteh <modtech7677@gmail.com>
2024-01-06 02:41:25 +0300

    search test

--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -22,12 +22,10 @@ router.get('/search/:query', async (req, res) => {
   try {
     const { query } = req.params;
 
-    if (!query) {
+    if (!query) { 
       return res.status(400).json({ message: 'Search query parameter is required.' });
     }
-
     const stations = await Station.find({ title: { $regex: new RegExp(query, 'i') } }).populate('chargers');
-
     res.json(stations);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -35,7 +33,6 @@ router.get('/search/:query', async (req, res) => {
 });
 
 
-// gets a list of stations by type
 router.get('/getStationsByType/:type', async (req, res) => {
   try {
     const { type } = req.params;
